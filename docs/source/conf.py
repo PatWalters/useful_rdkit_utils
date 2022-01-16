f7b72d221eefa77ab3b7a4db7ffb2c7dd16a5ce0
Pat Walters <wpwalters@gmail.com>
2022-01-16 15:44:20 -0500

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ copyright = f'{date.today().year}, Pat Walters'
 author = 'Pat Walters'
 
 # The full version, including alpha/beta/rc tags.
-release = '0.1.5'
+release = '0.1.6'
 
 # The short X.Y version.
 parsed_version = re.match(
