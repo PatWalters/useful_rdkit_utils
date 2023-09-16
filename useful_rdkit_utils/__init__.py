f0772dc851f0e6ca9b133fb517d66f7177051a00
Patrick Walters <pwalters@relaytx.com>
2023-09-16 15:56:54 -0400

    build system fixes

--- useful_rdkit_utils/__init__.py
+++ useful_rdkit_utils/__init__.py
@@ -7,12 +7,5 @@ from .pandas_utils import *
 from .ring_systems import *
 from .seaborn_utils import *
 
-# Handle versioneer
-from ._version import get_versions
-versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
-del get_versions, versions
+__version__ = "0.3.11"
 
-from . import _version
-__version__ = _version.get_versions()['version']
